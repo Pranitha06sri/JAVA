af8e73c6a1c12a553a67c9ea553837818e5c87ce
Pranitha06sri <prakashselvi@gmail.com>
2026-04-16 19:39:28 +0530

    commit

---
+++ Day 19/DeSerialization.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class DeSerialization {
+    public static void main(String[] args) throws Exception{
+        FileInputStream fi=new FileInputStream("student.txt");
+        ObjectInputStream ObjStream=new ObjectInputStream(fi);
+        //Student s=(Student) ObjStream.readObject();
+        //System.out.println(s.name+" "+s.mark);
+        while (true) { 
+            try {
+                Student s=(Student)ObjStream.readObject();
+                System.out.println(s.name+" "+s.mark);
+            } catch (EOFException e) {
+                break;
+            }
+        }
+        fi.close();
+        ObjStream.close();
+
+        
+    }
+    
+}
